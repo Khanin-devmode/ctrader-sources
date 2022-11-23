de161002e2bdcf28867ac508e297e60cb6eee499
Khanin <khanin.dev@gmail.com>
2022-11-23 20:06:12 +0700

    Implement rsi cross above/below threshold alert

--- Robots/Multipair_RSI_Alert_Bot/Multipair_RSI_Alert_Bot/Multipair_RSI_Alert_Bot.cs
+++ Robots/Multipair_RSI_Alert_Bot/Multipair_RSI_Alert_Bot/Multipair_RSI_Alert_Bot.cs
@@ -26,13 +26,13 @@ namespace cAlgo.Robots
         [Parameter(DefaultValue = 14, MinValue = 4, MaxValue = 30, Step = 2, Group = "RSI")]
         public int RsiPeriod { get; set; }
 
-        [Parameter(DefaultValue = 65, Group = "RSI")]
+        [Parameter(DefaultValue = 70, Group = "RSI")]
         public int RsiHighThres { get; set; }
 
-        [Parameter(DefaultValue = 35, Group = "RSI")]
+        [Parameter(DefaultValue = 30, Group = "RSI")]
         public int RsiLowThres { get; set; }
 
-        private List<string> symbolList = new List<string>() { "EURUSD", "GBPUSD", "USDCAD" };
+        private List<string> symbolList = new List<string>() { "EURUSD", "GBPUSD", "USDCAD","BTCUSD","ETHUSD","XAUUSD","USDJPY" };
         private List<PairInfo> pairInfoList = new List<PairInfo>();
 
         protected override void OnStart()
@@ -61,29 +61,52 @@ namespace cAlgo.Robots
 
         protected override void OnTick()
         {
+            foreach (PairInfo pair in pairInfoList)
+            {
 
+                var rsiResult = pair.rsi.Result;
+
+                if (rsiResult.HasCrossedBelow(RsiLowThres,0) )
+                { 
+                    telegram.SendTelegram(ChatID, BotToken, $"{TimeFrame} [{pair.pairName}] is crossing oversold down. RSI: {Math.Round(rsiResult.LastValue)}");
+                }
+                else if (rsiResult.HasCrossedAbove(RsiLowThres,0))
+                { 
+                    telegram.SendTelegram(ChatID, BotToken, $"{TimeFrame} [{pair.pairName}] is crossing oversold up. RSI: {Math.Round(rsiResult.LastValue)}");
+                } else if (rsiResult.HasCrossedBelow(RsiHighThres, 0))
+                {
+                    telegram.SendTelegram(ChatID, BotToken, $"{TimeFrame} [{pair.pairName}] is crossing overbought down. RSI: {Math.Round(rsiResult.LastValue)}");
+                }
+                else if (rsiResult.HasCrossedAbove(RsiHighThres, 0))
+                { 
+                    telegram.SendTelegram(ChatID, BotToken, $"{TimeFrame} [{pair.pairName}] is crossing overbought up. RSI: {Math.Round(rsiResult.LastValue)}");
+                }
+
+
+            }
 
 
         }
 
         protected override void OnBar()
         {
-            foreach (PairInfo pair in pairInfoList) {
+            //foreach (PairInfo pair in pairInfoList)
+            //{
 
-                var rsiResult = pair.rsi.Result;
+            //    var rsiResult = pair.rsi.Result;
 
-                if (rsiResult.Last(1) <= RsiLowThres)
-                { //Over Sold.
-                    telegram.SendTelegram(ChatID, BotToken, pair.pairName + " " + TimeFrame + " is oversold on last bar" + "rsi: "+rsiResult.Last(1));
-                }
-                else if (rsiResult.Last(1) >= RsiHighThres)
-                { //Over bought.
-                    telegram.SendTelegram(ChatID, BotToken, pair.pairName + " " +TimeFrame + " is overbought on last bar" + "rsi: " + rsiResult.Last(1));
-                }
+            //    if (rsiResult.Last(1) <= RsiLowThres)
+            //    { //Over Sold.
+            //        telegram.SendTelegram(ChatID, BotToken, $"{TimeFrame} [{pair.pairName}] is oversold on last bar. RSI: {Math.Round(rsiResult.Last(1))}");
+            //    }
+            //    else if (rsiResult.Last(1) >= RsiHighThres)
+            //    { //Over bought.
+            //        telegram.SendTelegram(ChatID, BotToken, $"{TimeFrame} [{pair.pairName}] is overbought on last bar. RSI: {Math.Round(rsiResult.Last(1))}");
+            //    }
 
 
-            }
-                
+            //}
+
         }
 
         protected override void OnStop()
