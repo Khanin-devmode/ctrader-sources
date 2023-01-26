c19baddbecfbca0a4b7dcbc490f50abc0537ac36
Khanin <khanin.dev@gmail.com>
2023-01-26 17:48:42 +0700

    Draw Daytrend resistance

--- Robots/Support  Resistance Bot/Support  Resistance Bot/Support  Resistance Bot.cs
+++ Robots/Support  Resistance Bot/Support  Resistance Bot/Support  Resistance Bot.cs
@@ -15,27 +15,51 @@ namespace cAlgo.Robots
         [Parameter(DefaultValue = "Hello world!")]
         public string Message { get; set; }
 
+        Bars dBars;
+        int n;
         protected override void OnStart()
         {
             // To learn more about cTrader Automate visit our Help Center:
             // https://help.ctrader.com/ctrader-automate
 
-            Print(Message);
+            dBars = MarketData.GetBars(TimeFrame.Daily);
+            n = dBars.Count;
             
-            var length = Chart.MaxVisibleBars;
-            
-            for (var i = 1 ; i< length; i++){
-            
-               if( i > 2){
-                    if(IsResistance(i)){
-                       Print($"Bars {i} is resistance");
+            Print(n);
+
+            for (int i = 100; i >= 3; i--)
+            {
+                Print(dBars.Last(i).OpenTime);
+                if (i < (n - 2))
+                {
+                    //if (IsResistance(i))
+                    //{
+                    //    Print($"Bars {i} is resistance");
+                    //    Print();
+                    //    Chart.DrawHorizontalLine($"Resistance Line {i}", Bars[i].High, Color.Red);
+                    //    Print(Bars[i].OpenTime);
+
+
+                    //}
+                    var cond1 = dBars.Last(i).High > dBars.Last(i - 1).High;
+                    var cond2 = dBars.Last(i-1).High > dBars.Last(i - 2).High;
+
+                    var cond3 = dBars.Last(i).High > dBars.Last(i + 1).High;
+                    var cond4 = dBars.Last(i + 1).High > dBars.Last(i + 2).High;
+
+                    if (cond1 && cond2 && cond3 && cond4) {
+                        Chart.DrawHorizontalLine($"Resistance Line {i}", dBars.Last(i).High, Color.Red);
+                        
                     }
+
                 }
 
             }
-            
-            
-            
+
+
+
+
+
         }
 
         protected override void OnTick()
@@ -47,19 +71,17 @@ namespace cAlgo.Robots
         {
             // Handle cBot stop here
         }
-        
-        protected bool IsResistance(int i){
-            
-            Print(i);
-        
-            bool cond1 = Bars[i].High > Bars[i-1].High;
-            bool cond2 = Bars[i-1].High > Bars[i-2].High;
-            
-            bool cond3 = Bars[i].High > Bars[i+1].High;
-            bool cond4 = Bars[i+1].High > Bars[i+2].High;
-            
+
+        protected bool IsResistance(int i)
+        {
+            bool cond1 = Bars[i].High > Bars[i - 1].High;
+            bool cond2 = Bars[i - 1].High > Bars[i - 2].High;
+
+            bool cond3 = Bars[i].High > Bars[i + 1].High;
+            bool cond4 = Bars[i + 1].High > Bars[i + 2].High;
+
             return cond1 && cond2 && cond3 && cond4;
-        
+
         }
     }
 }
